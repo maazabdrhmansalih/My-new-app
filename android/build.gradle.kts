6af148d7810ec9ba968421a32e406acd97264898
maazabdrhmansalih <maazabdrhman@gmail.com>
2025-08-10 16:22:43 -1000

    تعديلات 1

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,23 +1,17 @@
-pluginManagement {
-    val flutterSdkPath = run {
-        val properties = java.util.Properties()
-        file("local.properties").inputStream().use { properties.load(it) }
-        properties.getProperty("flutter.sdk")
-            ?: throw GradleException("flutter.sdk not set in local.properties")
-    }
-
+buildscript {
     repositories {
         google()
         mavenCentral()
-        gradlePluginPortal()
-        maven { url = uri("$flutterSdkPath/packages/flutter_tools/gradle") }
     }
-
-    plugins {
-        id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-        id("com.android.application") version "8.2.0" apply false
-        id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
 
-include(":app")
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
