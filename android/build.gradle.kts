966fe33742327941dad1f38e9d4ae741f173de53
maazabdrhmansalih <maazabdrhman@gmail.com>
2025-08-10 15:44:12 -1000

    تعديلات

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,22 +1,23 @@
+pluginManagement {
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        file("local.properties").inputStream().use { properties.load(it) }
+        properties.getProperty("flutter.sdk")
+            ?: throw GradleException("flutter.sdk not set in local.properties")
+    }
 
-allprojects {
     repositories {
         google()
         mavenCentral()
+        gradlePluginPortal()
+        maven { url = uri("$flutterSdkPath/packages/flutter_tools/gradle") }
+    }
+
+    plugins {
+        id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+        id("com.android.application") version "8.2.0" apply false
+        id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-}
+include(":app")
--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,18 +1,23 @@
 pluginManagement {
     val flutterSdkPath = run {
         val properties = java.util.Properties()
-        file("local.properaties");
+        file("local.properties").inputStream().use { properties.load(it) }
+        properties.getProperty("flutter.sdk")
+            ?: throw GradleException("flutter.sdk not set in local.properties")
+    }
+
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven { url = uri("$flutterSdkPath/packages/flutter_tools/gradle") }
     }
-}
 
-plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    plugins {
+        id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+        id("com.android.application") version "8.2.0" apply false
+        id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    }
 }
 
 include(":app")
